cc108df58d2409e42ffb403b4623fb970d663c30
Martial Dufour <mdufour8@msn.com>
2022-07-21 23:26:39 -0400

    Testing no change

--- FileSupport/VBArray.vb
+++ FileSupport/VBArray.vb
@@ -1,48 +1,49 @@
 ﻿Namespace ExtensionService
-  Public Class VBArray(Of T)
-    Private ThisLowBound As Integer
-    Private ThisHighBound As Integer
 
-    Private ThisArray() As T
+	Public Class VBArray(Of T)
+		Private ThisLowBound As Integer
+		Private ThisHighBound As Integer
 
-    Public Sub New(ByVal LBound As Integer, ByVal UBound As Integer)
-      Me.ReDim(LBound, UBound)
-    End Sub
+		Private ThisArray() As T
 
-    Public ReadOnly Property LBound As Integer
-      Get
-        Return ThisLowBound
-      End Get
-    End Property
+		Public Sub New(ByVal LBound As Integer, ByVal UBound As Integer)
+			Me.ReDim(LBound, UBound)
+		End Sub
 
-    Public ReadOnly Property UBound As Integer
-      Get
-        Return ThisHighBound
-      End Get
-    End Property
+		Public ReadOnly Property LBound As Integer
+			Get
+				Return ThisLowBound
+			End Get
+		End Property
 
-    Public Sub [ReDim](ByVal LBound As Integer, ByVal UBound As Integer)
-      ThisLowBound = LBound
-      ThisHighBound = UBound
-      ReDim ThisArray(0 To (ThisHighBound - ThisLowBound))
-    End Sub
+		Public ReadOnly Property UBound As Integer
+			Get
+				Return ThisHighBound
+			End Get
+		End Property
 
-    Public Function ToArray() As T()
-      Return ThisArray
-    End Function
+		Public Sub [ReDim](ByVal LBound As Integer, ByVal UBound As Integer)
+			ThisLowBound = LBound
+			ThisHighBound = UBound
+			ReDim ThisArray(0 To (ThisHighBound - ThisLowBound))
+		End Sub
 
-    Default Public Property Item(ByVal Index As Integer) As T
-      Get
-        Return ThisArray(Index - ThisLowBound)
-      End Get
+		Public Function ToArray() As T()
+			Return ThisArray
+		End Function
 
-      Set(ByVal Value As T)
-        ThisArray(Index - ThisLowBound) = Value
-      End Set
-    End Property
+		Default Public Property Item(ByVal Index As Integer) As T
+			Get
+				Return ThisArray(Index - ThisLowBound)
+			End Get
 
-    Public Overrides Function ToString() As String
-      Return String.Format("{0}({1} to {2})", TypeName(Me), Me.LBound, Me.UBound)
-    End Function
-  End Class
+			Set(ByVal Value As T)
+				ThisArray(Index - ThisLowBound) = Value
+			End Set
+		End Property
+
+		Public Overrides Function ToString() As String
+			Return String.Format("{0}({1} to {2})", TypeName(Me), Me.LBound, Me.UBound)
+		End Function
+	End Class
 End Namespace
