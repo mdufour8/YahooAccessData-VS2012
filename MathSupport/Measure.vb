d7402f50e9a1301425c309083a5adddfa127be94
mdufour8 <mdufour8@msn.com>
2023-02-20 13:19:42 -0500

    Minor modification with no consequence

--- MathSupport/Measure.vb
+++ MathSupport/Measure.vb
@@ -1,4 +1,5 @@
 ﻿Imports MathNet.Numerics
+Imports YahooAccessData.OptionValuation
 
 Namespace MathPlus
 #Region "Measure"
@@ -285,6 +286,67 @@ Namespace MathPlus
           Return OptionStrikePrice * Math.Exp(-RiskFreeRate * TimeToExpirationInYear) * ThisCDFOf_Negd2 - StockPrice * Math.Exp(-DividendRate * TimeToExpirationInYear) * ThisCDFOf_Negd1
         End If
       End Function
+
+      ''' <summary>
+      ''' Return the expected Stock Price value given the option price of the asset
+      ''' </summary>
+      ''' <param name="OptionType"></param>
+      ''' <param name="OptionPrice"></param>
+      ''' <param name="OptionStrikePrice"></param>
+      ''' <param name="TimeToExpirationInYear"></param>
+      ''' <param name="RiskFreeRate"></param>
+      ''' <param name="DividendRate"></param>
+      ''' <param name="VolatilityPerYear"></param>
+      ''' <returns></returns>
+      Public Shared Function BlackScholesToStockPrice(
+        ByVal OptionType As enuOptionType,
+        ByVal OptionPrice As Double,
+        ByVal OptionStrikePrice As Double,
+        ByVal TimeToExpirationInYear As Double,
+        ByVal RiskFreeRate As Double,
+        ByVal DividendRate As Double,
+        ByVal VolatilityPerYear As Double) As Double
+
+        Throw New NotImplementedException
+        'Dim d1 As Double, d2 As Double
+        'Dim ThisNormal = New MathNet.Numerics.Distributions.Normal(mean:=0, stddev:=1)
+        ''DividendRate = 0
+
+
+        'd1 = (Math.Log(StockPrice / OptionStrikePrice) + (RiskFreeRate - DividendRate + VolatilityPerYear ^ 2 / 2) * TimeToExpirationInYear) / (VolatilityPerYear * Math.Sqrt(TimeToExpirationInYear))
+        'd2 = d1 - VolatilityPerYear * Math.Sqrt(TimeToExpirationInYear)
+
+        ''d1 = (Log(S / x) + ( b + v ^ 2 / 2) * T) / (v * Sqr(T))
+        ''d2 = d1 - v * Sqr(T)
+
+        ''If CallPutFlag = "c" Then
+        ''  GBlackScholes = S * Exp((b - r) * T) * CND(d1) - x * Exp(-r * T) * CND(d2)
+        ''ElseIf CallPutFlag = "p" Then
+        ''  GBlackScholes = x * Exp(-r * T) * CND(-d2) - S * Exp((b - r) * T) * CND(-d1)
+        ''End If
+        ''C = Se−qTN(d1)−Ke−rTN(d2)
+        ''P = Ke−rTN(−d2)−Se−qTN(d1)
+        ''Where q Is the known dividend yield And
+        ''d1 = ln(SK) + T(r−q+σ22)σT−−√
+        ''d2 = ln(SK) + T(r−q−σ22)σT−−√=d1−σT−−√
+        ''One possible implementation (a rough And unoptimized 
+
+        ''Note:  b = RiskFreeRate - DividendRate
+
+        'Dim ThisCDFOf_d1 As Double = ThisNormal.CumulativeDistribution(d1)
+        'Dim ThisCDFOf_Negd1 As Double = ThisNormal.CumulativeDistribution(-d1)
+        'Dim ThisCDFOf_d2 As Double = ThisNormal.CumulativeDistribution(d2)
+        'Dim ThisCDFOf_Negd2 As Double = ThisNormal.CumulativeDistribution(-d2)
+
+        'If OptionType = enuOptionType._Call Then
+        '  Return StockPrice * Math.Exp(-DividendRate * TimeToExpirationInYear) * ThisCDFOf_d1 - OptionStrikePrice * Math.Exp(-RiskFreeRate * TimeToExpirationInYear) * ThisCDFOf_d2
+        'Else
+        '  Return OptionStrikePrice * Math.Exp(-RiskFreeRate * TimeToExpirationInYear) * ThisCDFOf_Negd2 - StockPrice * Math.Exp(-DividendRate * TimeToExpirationInYear) * ThisCDFOf_Negd1
+        'End If
+
+
+      End Function
+
 
       ''' <summary>
       ''' Delta is the amount an option price is expected to move based on a $1 change 
